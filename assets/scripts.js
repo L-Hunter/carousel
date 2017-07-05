05f96dcfe30c683450dcbd63ee8bd90b00497f69
TheButlerDidIt <labutler@gmail.com>
2017-07-05 16:23:36 -0400

    outlines approach

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,14 @@
-//js
+
+//add event listeners tied to html arrows and next and previous functions
+
+//advance slide show, Make the transitions smooth using simple effects
+function next(){
+
+}
+
+//reverse slide show, Make the transitions smooth using simple effects
+function previous(){
+
+}
+
+//Add a timeout which advances the slides every 5 seconds.
