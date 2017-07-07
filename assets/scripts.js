6341e9c0b256fa724cdec9349d3df5cefb62190d
TheButlerDidIt <labutler@gmail.com>
2017-07-06 22:21:17 -0400

    chipping away slowly

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,23 @@
 
-//add event listeners tied to html arrows and next and previous functions
+document.getElementById("display").style.backgroundImage = "url('assets/images/beach.jpg')";
+var backward = document.getElementById("backward");
+var forward = document.getElementById("forward");
 
+backward.addEventListener("click", previous);
+forward.addEventListener("click", next)
 //advance slide show, Make the transitions smooth using simple effects
+
+var count = 0
+var pictures = ['assets/images/beach.jpg', 'assets/images/grass.jpg','assets/images/snowyforest.jpg']
+
 function next(){
+	console.log("show the next picture");
 
 }
 
 //reverse slide show, Make the transitions smooth using simple effects
 function previous(){
-
+	console.log("show the previous picture");
 }
 
 //Add a timeout which advances the slides every 5 seconds.
