5505a2283d8b037e09a87a4d14b508d88088d81f
TheButlerDidIt <labutler@gmail.com>
2017-07-17 15:52:58 -0400

    advances and rewinds slideshow

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,36 @@
 
-document.getElementById("display").style.backgroundImage = "url('assets/images/beach.jpg')";
+var display = document.getElementById("display");
 var backward = document.getElementById("backward");
 var forward = document.getElementById("forward");
+var count = 0;
+var pictures = ['assets/images/beach.jpg', 'assets/images/grass.jpg','assets/images/snowyforest.jpg']
 
 backward.addEventListener("click", previous);
 forward.addEventListener("click", next)
-//advance slide show, Make the transitions smooth using simple effects
+//Make the transitions smooth using simple effects
 
-var count = 0
-var pictures = ['assets/images/beach.jpg', 'assets/images/grass.jpg','assets/images/snowyforest.jpg']
+display.style.backgroundImage = "url('" + pictures[count]+ "')";
+
 
 function next(){
 	console.log("show the next picture");
-
+	if (count == pictures.length - 1){
+		count = 0;
+	} else {
+		count ++;	
+	}
+	display.style.backgroundImage = "url('" + pictures[count]+ "')";
 }
 
-//reverse slide show, Make the transitions smooth using simple effects
+//Make the transitions smooth using simple effects
 function previous(){
 	console.log("show the previous picture");
+	if (count == 0){
+		count = pictures.length - 1;
+	} else {
+		count --;
+	}
+	display.style.backgroundImage = "url('" + pictures[count]+ "')";
 }
 
 //Add a timeout which advances the slides every 5 seconds.
