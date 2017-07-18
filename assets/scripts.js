6f0f34f8fd61e9f0e93dfa0e9b46c2661c421da1
TheButlerDidIt <labutler@gmail.com>
2017-07-18 15:53:35 -0400

    attempts to fix transition

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,21 +6,19 @@ var count = 0;
 var pictures = ['assets/images/beach.jpg', 'assets/images/grass.jpg','assets/images/snowyforest.jpg', 'assets/images/ozarks.jpg', 'assets/images/nasa.jpg'];
 
 backward.addEventListener("click", previous);
-forward.addEventListener("click", next)
+forward.addEventListener("click", next);
 
-display.style.transition = "all 1s"
 display.style.backgroundImage = "url('" + pictures[count]+ "')";
 
-
 setInterval(function(){
-	next();}, 5000);
+	next(); }, 5500);
 
 function next(){
 	console.log("show the next picture");
 	if (count == pictures.length - 1){
 		count = 0;
 	} else {
-		count ++;	
+		count++;	
 	}
 	display.style.backgroundImage = "url('" + pictures[count]+ "')";
 }
@@ -30,7 +28,7 @@ function previous(){
 	if (count == 0){
 		count = pictures.length - 1;
 	} else {
-		count --;
+		count--;
 	}
 	display.style.backgroundImage = "url('" + pictures[count]+ "')";
 }
