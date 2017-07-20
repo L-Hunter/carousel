e8f9e9fa1689c7efe7e1929e7836e3febe485292
TheButlerDidIt <labutler@gmail.com>
2017-07-19 21:40:49 -0400

    experimenting with navigation dots

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,11 +2,18 @@
 var display = document.getElementById("display");
 var backward = document.getElementById("backward");
 var forward = document.getElementById("forward");
+var n0 = document.getElementById("n0");
+var n1 = document.getElementById("n1");
+var n2 = document.getElementById("n2");
+var n3 = document.getElementById("n3");
+var n4 = document.getElementById("n4");
+
 var count = 0;
 var pictures = ['assets/images/beach.jpg', 'assets/images/grass.jpg','assets/images/snowyforest.jpg', 'assets/images/ozarks.jpg', 'assets/images/nasa.jpg'];
 
 backward.addEventListener("click", previous);
 forward.addEventListener("click", next);
+n1.addEventListener("click", selected);
 
 display.style.backgroundImage = "url('" + pictures[count]+ "')";
 
@@ -31,4 +38,10 @@ function previous(){
 		count--;
 	}
 	display.style.backgroundImage = "url('" + pictures[count]+ "')";
+}
+
+function selected(){
+	n1.innerHTML = "&#9673;";
+	display.style.backgroundImage = "url('" + pictures[1]+ "')";
+	n0.innerHTML = "&#9678;";
 }
